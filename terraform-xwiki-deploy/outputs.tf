1f20d191480dc9405c6d76abf66130658d303b2a
Mojtaba Yousefi <mojtaba.yousefi@live.com>
2024-09-08 11:29:17 -0400

    feat: Add Terraform outputs for XWiki deployment
    This change adds a new `outputs.tf` file to the `terraform-xwiki-deploy` directory, which defines various output values for the XWiki deployment. These outputs include the resource group name, App Service name and URL, PostgreSQL server details, storage account information, and Application Insights details. This will make it easier to access and reference these values after the deployment is complete.

---
+++ terraform-xwiki-deploy/outputs.tf
@@ -0,0 +1,40 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.xwiki_rg.name
+}
+
+output "app_service_name" {
+  value = azurerm_app_service.xwiki_app.name
+}
+
+output "app_service_url" {
+  value = "https://${azurerm_app_service.xwiki_app.default_site_hostname}"
+}
+
+output "postgresql_server_name" {
+  value = azurerm_postgresql_flexible_server.xwiki_db.name
+}
+
+output "postgresql_server_fqdn" {
+  value = azurerm_postgresql_flexible_server.xwiki_db.fqdn
+}
+
+output "postgresql_database_name" {
+  value = azurerm_postgresql_flexible_server_database.xwiki_db.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.xwiki_storage.name
+}
+
+output "backup_container_name" {
+  value = azurerm_storage_container.xwiki_backups.name
+}
+
+output "application_insights_name" {
+  value = azurerm_application_insights.xwiki_insights.name
+}
+
+output "application_insights_instrumentation_key" {
+  value     = azurerm_application_insights.xwiki_insights.instrumentation_key
+  sensitive = true
+}
